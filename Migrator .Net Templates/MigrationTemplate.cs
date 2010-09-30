f74377573589ca8d9b1413672a026ad997be621b
Rob White <rob.j.white@gmail.com>
2010-09-30 20:42:20 +0100

    commit old code

---
+++ Migrator .Net Templates/MigrationTemplate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TemplateWizard;
+using EnvDTE;
+
+namespace Migrator.Net_Templates
+{
+    public class MigrationTemplate : IWizard
+    {
+        // Methods
+        public void BeforeOpeningFile(ProjectItem projectItem)
+        {
+        }
+
+        public void ProjectFinishedGenerating(Project project)
+        {
+        }
+
+        public void ProjectItemFinishedGenerating(ProjectItem projectItem)
+        {
+        }
+
+        public void RunFinished()
+        {
+        }
+
+        public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
+        {
+            replacementsDictionary.Add("$migrationId$", string.Format("{0:yyyyMMddhhssfff}", DateTime.Now));
+        }
+
+        public bool ShouldAddProjectItem(string filePath)
+        {
+            return false;
+        }
+    }
+}
